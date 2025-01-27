bbc0a6480343146fedfc52d73af70045f34b9983
Ashish Jain <32389919+ashishjain2412@users.noreply.github.com>
2025-01-27 16:20:32 +0530

    Create lab.sh
    Created lab.sh for qwiklab- to be used only by qwiklab.

---
+++ chaostoolkit-examples/l7ilb-urlmap-fault-injection/scripts/lab.sh
@@ -0,0 +1,26 @@
+jq 'del(.rollbacks)' ../chaos-experiment/config/experiment.json >../chaos-experiment/config/o_experiment.json
+mv ../chaos-experiment/config/o_experiment.json ../chaos-experiment/config/experiment.json
+
+sudo chmod -R 777 ~/chaos-engineering
+
+student_user=$(gcloud config get-value account)
+
+gcloud projects add-iam-policy-binding ${GOOGLE_CLOUD_PROJECT} \
+--member="user:${student_user}" \
+--role='roles/iam.serviceAccountTokenCreator'
+
+gcloud projects add-iam-policy-binding ${GOOGLE_CLOUD_PROJECT} \
+--member="user:${student_user}" \
+--role='roles/iam.serviceAccountUser'
+
+gcloud projects add-iam-policy-binding ${GOOGLE_CLOUD_PROJECT} \
+--member="user:${student_user}" \
+--role='roles/serviceusage.serviceUsageAdmin'
+
+gcloud projects add-iam-policy-binding ${GOOGLE_CLOUD_PROJECT} \
+--member="user:${student_user}" \
+--role='roles/iap.tunnelResourceAccessor'
+
+gsutil mb gs://${GOOGLE_CLOUD_PROJECT}-terraform-backend
+sudo mkdir -p /opt/chaostoolkit-examples/
+sudo chmod -R 777 /opt/chaostoolkit-examples
